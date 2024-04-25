187145f30dd37031990cbc46a7416528f03b9c59
davidizuchukwu <167909416+davidizuchukwu@users.noreply.github.com>
2024-04-25 04:46:48 -0700

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,36 +2,35 @@
 {
 let slideindex=0;
     const slides = document.getElementsByClassName('slide');
-    function showslides(n) {
-      if (n<0) {
-        slideindex = slides.length -1
+    function showslide(n) {
+      slideindex=n;
+      if(slideindex<0){
+        slideindex= slide.length-1;
+      }else if (slideindex>=slides.length){
+        slideindex=0
       }
-      else if (n>=slides.length){
-        slideindex = 0;
-      } else {
-        slideindex= n
+      for(let i=0; i<slides.length; i++){
+        slides[i].style.display="none"
       }
-      for (let i = 0; i<slides.length; i= i +1){
-        slides[i].style.display = "none";
-      }
-      slides[slideindex].style.display = "block";
+      slides[slideindex].style.display="block";
+      slides[slideindex].style.left="0";
     }
-   
+
 
     function nextSlide() {
-      showslides(slideindex +1);
+      showslide(slideindex +1);
     }
     function prevSlide() {
-      showslides(slideindex -1);
+      showslide(slideindex -1);
     }
     function autoSlide() {
       nextSlide()
-      setTimeout(autoSlide,2000)
+      setTimeout(autoSlide,2000);
     }
     
 
     document.addEventListener("DOMContentLoaded",function() {
-      showslides(slideindex);
+      showslide(slideindex);
       autoSlide();
 
       const prevButton = document.querySelector(".prev")
@@ -46,7 +45,17 @@ let slideindex=0;
     });
   });
 
+  prevButton.addEventListener("touchstart",function(e){
+    e.preventDefault();
+    prevSlide();
+  });
+
+  nextButton.addEventListener("touchstart", function(e){
+    e.preventDefault();
+    nextSlide();
+  })
 }
+
 // testimonials//
 {
   let slideindex=0;
